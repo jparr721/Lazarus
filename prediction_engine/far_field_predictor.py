06a38385f3967dfb9a937d8bd2fc49c24c57c2de
Jarred Parr <jparr721@gmail.com>
2018-08-29 12:46:46 -0400

    clean out old model

--- prediction_engine/far_field_predictor.py
+++ prediction_engine/far_field_predictor.py
@@ -1,24 +1,11 @@
 import pandas as pd
-from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+
 
 diabetes_data = pd.read_csv('../Diabetes-Data/diabetes.csv')
-target = diabetes_data.Outcome
-predictors = diabetes_data.drop(['Outcome'], axis=1)
-
-X_train, X_test, y_train, y_test = train_test_split(predictors,
-                                                    target, train_size=0.7,
-                                                    test_size=0.3,
-                                                    random_state=0)
+y = diabetes_data.Outcome
+X = diabetes_data.drop(['Outcome'], axis=1)
 
 
-# Score our dataset to see how it stacks up with a given algo
-def score_dataset(X_train, X_test, y_train, y_test):
-    model = RandomForestRegressor()
-    model.fit(X_train, y_train)
-    predictions = model.predict(X_test)
-    return mean_absolute_error(y_test, predictions)
-
-
-print(score_dataset(X_train, X_test, y_train, y_test))
