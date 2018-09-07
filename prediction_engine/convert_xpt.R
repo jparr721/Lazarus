02d492a33f8589ac5b4204eeb9563b73e6836e14
Jarred Parr <jparr721@gmail.com>
2018-09-07 13:51:25 -0400

    added file conversion tool

---
+++ prediction_engine/convert_xpt.R
@@ -0,0 +1,20 @@
+library(readr)
+library(SASxport)
+
+setwd("/home/hermes/ML/Lazarus/Diabetes-Data")
+
+xptfiles <- list.files(pattern = "*.XPT")
+csvfiles <- NULL
+
+for (i in 1:length(xptfiles)) {
+  xptfile <- xptfiles[i]
+  file_name <- substr(xptfile, 1, nchar(xptfile) - 3)
+  ext <- "csv"
+
+  csvfile <- pase0(file_name, ext)
+  csvfiles[i] <- paste0(file_name, ext)
+
+  df <- read.xport(xptfile)
+  write.csv(df, file = csvfile, row.names = FALSE)
+  cat(c(paste0("[", i, "]"), csvfiles[i], '\n'))
+}
