9a0725a9d8e9a97eff4b6b944a272d75bb339279
Jarred Parr <jparr721@gmail.com>
2018-09-07 11:03:20 -0400

    add init

--- prediction_engine/predictor.py
+++ prediction_engine/predictor.py
@@ -1,7 +1,11 @@
 import tensorflow as tf
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 
-def predictor():
-
+class Predictor(object):
+    def __init__(self, batch_size=50, gamma=-10):
+        self.batch_size = batch_size
+        self._sess = tf.Session()
+        self.gamma = gamma
