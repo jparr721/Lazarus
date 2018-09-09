e347967501c4c209290df7ee4c90404e3bb68e5a
Jarred Parr <jparr721@gmail.com>
2018-09-09 18:02:45 -0400

    implemented nonlinear kernel svm in tensorflow

--- prediction_engine/base.py
+++ prediction_engine/base.py
@@ -4,6 +4,7 @@ Base classes and modules for certain repetitive tasks
 import datetime
 from matplotlib.colors import ListedColormap
 from numpy import np
+import tensorflow as tf
 import matplotlib.pyplot as plt
 
 
@@ -13,43 +14,57 @@ class Util(object):
         self.y = y,
         self.classifier = classifier
 
-        def plot_decision_regions(self, test_idx, resolution=0.02):
-            """
-            Makes a color coded plot of whatever
-            learning model you inject
-            """
+    def plot_decision_regions(self, test_idx, resolution=0.02):
+        """
+        Makes a color coded plot of whatever
+        learning model you inject. This works
+        only for sklearn classifiers
+        """
 
-            # Marker generator and color map
-            markers = ('s', 'x', 'o', '^', 'v')
-            colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
-            cmap = ListedColormap(colors[:len(np.unique(y))])
+        # Marker generator and color map
+        markers = ('s', 'x', 'o', '^', 'v')
+        colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+        cmap = ListedColormap(colors[:len(np.unique(self.y))])
 
-            # Plot decision surface
-            x1_min, x1_max = self.X[:, 0].min() - 1, self.X[:, 0].max() + 1
-            x2_min, x2_max = self.X[:, 1].min() - 1, self.X[:, 1].max() + 1
-            xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
-                                   np.arange(x2_min, x2_max, resolution))
-            z = self.classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
-            z = z.reshape(xx1.shape)
-            plt.contourf(xx1, xx2, z, alpha=0.3, cmap=cmap)
-            plt.xlim(xx1.min(), xx1.max())
-            plt.ylim(xx2.min(), xx2.max())
+        # Plot decision surface
+        x1_min, x1_max = self.X[:, 0].min() - 1, self.X[:, 0].max() + 1
+        x2_min, x2_max = self.X[:, 1].min() - 1, self.X[:, 1].max() + 1
+        xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                               np.arange(x2_min, x2_max, resolution))
+        z = self.classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+        z = z.reshape(xx1.shape)
+        plt.contourf(xx1, xx2, z, alpha=0.3, cmap=cmap)
+        plt.xlim(xx1.min(), xx1.max())
+        plt.ylim(xx2.min(), xx2.max())
 
-            for idx, cl in enumerate(np.unique(y)):
-                plt.scatter(x=self.X[y == cl, 0], y=self.X[y == cl, 1],
-                            alpha=0.8, c=colors[idx],
-                            marker=markers[idx], label=cl,
-                            edgecolor='black')
+        for idx, cl in enumerate(np.unique(self.y)):
+            plt.scatter(x=self.X[self.y == cl, 0], y=self.X[self.y == cl, 1],
+                        alpha=0.8, c=colors[idx],
+                        marker=markers[idx], label=cl,
+                        edgecolor='black')
 
-            # highlight test samples
-            if test_idx:
-                # Plot all samples
-                self.X_test = self.X[test_idx, :]
+        # highlight test samples
+        if test_idx:
+            # Plot all samples
+            self.X_test = self.X[test_idx, :]
 
-                plt.scatter(self.X_test[:, 0], self.X_test[:, 1],
-                            c='', edgecolor='black', alpha=1.0,
-                            linewidth=1, marker='o',
-                            s=100, label='Test Set')
+            plt.scatter(self.X_test[:, 0], self.X_test[:, 1],
+                        c='', edgecolor='black', alpha=1.0,
+                        linewidth=1, marker='o',
+                        s=100, label='Test Set')
+
+        def plot_tf_classifier_model(self, batch_size):
+            '''
+            Creates a color grid with
+            data and allows for visualizing
+            of tensorflow classifiers
+            '''
+            X = tf.placeholder(shape=[None, 2], dtype=tf.float32)
+            y = tf.placeholder(shape=[None, 1], dtype=tf.float32)
+
+            prediction_grid = tf.placeholder(shape=[None, 2], dtype=tf.float32)
+
+            b = tf.Variable(tf.random_normal(shape=[1, batch_size]))
 
 
 class Logger(object):
--- prediction_engine/predictor.py
+++ prediction_engine/predictor.py
@@ -5,7 +5,102 @@ import matplotlib.pyplot as plt
 
 
 class Predictor(object):
-    def __init__(self, batch_size=50, gamma=-10):
+    def __init__(self, batch_size, gamma):
         self.batch_size = batch_size
         self._sess = tf.Session()
         self.gamma = gamma
+        self.prediction_grid = tf.placeholder(shape=[None, 2], dtype=tf.float32)
+
+    def read_data(self, source):
+        '''
+        Takes the absolute path to some data
+        and outputs a new pandas dataframe
+        '''
+        return pd.read_csv(source)
+
+    def fit(self, X, y, b):
+        distribution = tf.reduce_sum(tf.square(X), 1)
+        distribution = tf.reshape(distribution, [-1, 1])
+
+        squared_distributions = \
+            tf.add(tf.sub(distribution,
+                          tf.mul(2., tf.matmul(X, tf.transpose(X))),
+                          tf.transpose(distribution)))
+
+        kernel = tf.exp(tf.mul(self.gamma, tf.abs(squared_distributions)))
+
+        model_output = tf.matmul(b, kernel)
+
+        first_term = tf.reduce_sum(b)
+        b_vec_cross = tf.matmul(tf.transpose(b), b)
+
+        y_target_cross = tf.matmul(y, tf.transpose(y))
+        second_term = tf.reduce_sum(tf.mul(kernel, tf.mul(b_vec_cross,
+                                                          y_target_cross)))
+        # maximize the negative loss function
+        loss = tf.neg(tf.sub(first_term, second_term))
+
+        return loss, model_output
+
+    def predict(self, X, y, b):
+        rA = tf.reshape(tf.reduce_sum(tf.square(X), 1), [-1, 1])
+        rB = tf.reshape(tf.reduce_sum(tf.square(self.prediction_grid), 1), [-1, 1])
+
+        pred_sq_dist = \
+            tf.add(tf.sub(rA,
+                          tf.mul(2.,
+                                 tf.matmul(X,
+                                           tf.transpose(self.prediction_grid))),
+                          tf.transpose(rB)))
+
+        # linear prediction kernel
+        # pred_kernel = tf.matmul(X, tf.transpose(prediction_grid))
+        pred_kernel = tf.exp(tf.mul(self.gamma, tf.abs(pred_sq_dist)))
+
+        prediction_output = tf.matmul(tf.mul(tf.transpose(y), b), pred_kernel)
+
+        prediction = tf.sign(prediction_output -
+                             tf.reduce_mean(prediction_output))
+
+        accuracy = tf.reduce_mean(tf.cast(tf.equal(tf.squeeze(prediction),
+                                                   tf.squeeze(y)), tf.float32))
+
+        return accuracy
+
+    def optimize(self, X, y, b, loss, optimizer):
+        '''
+        Optimizer function to
+        minimize the cost of the
+        svm algorithm
+        '''
+        accuracy = self.predict(X, y, b)
+        training_step = optimizer.minimize(loss)
+
+        init = tf.initialize_all_variables()
+        self._sess.run(init)
+
+        # The training loop
+        loss_vector = []
+        batch_accuracy = []
+
+        for i in range(self.batch_size):
+            rand_index = np.random.choice(len(X), size=self.batch_size)
+            rand_x = X[rand_index]
+            rand_y = y[rand_index]
+
+            temp_loss = self._sess.run(training_step,
+                                       feed_dict={X: rand_x, y: rand_y})
+
+            loss_vector.append(temp_loss)
+
+            acc_temp = self._sess.run(accuracy,
+                                      feed_dict={X: rand_x,
+                                                 y: rand_y,
+                                                 self.prediction_grid: rand_x})
+
+            batch_accuracy.append(acc_temp)
+
+            # Update output every 100 runs
+            if (i+1) % 100 == 0:
+                print('Step: {}'.format(str(i+1)))
+                print('Loss = '.format(str(temp_loss)))
