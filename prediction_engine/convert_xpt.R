7205e81c907c74251baabf5a08b6ec5895f4988c
Jarred Parr <jparr721@gmail.com>
2018-09-17 08:04:33 -0400

    FIxed the path

--- prediction_engine/convert_xpt.R
+++ prediction_engine/convert_xpt.R
@@ -1,7 +1,7 @@
 library(readr)
 library(SASxport)
 
-setwd("/home/hermes/ML/Lazarus/Diabetes-Data/csvs")
+setwd("~/ML/Lazarus/Diabetes-Data/csvs")
 
 xptfiles <- list.files(pattern = "*.XPT")
 csvfiles <- NULL
