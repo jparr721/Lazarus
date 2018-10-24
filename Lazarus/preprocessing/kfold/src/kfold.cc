6297191faf5ded64f4db432745ecdc9b6f84cd97
Jarred Parr <jparr721@gmail.com>
2018-10-24 12:40:22 -0400

    it makes the cmake files, but linker is wrong

--- Lazarus/preprocessing/kfold/src/kfold.cc
+++ Lazarus/preprocessing/kfold/src/kfold.cc
@@ -1,19 +1,19 @@
-#include <kfold/KFold.hpp>
+#include <kfold/kfold.hpp>
 
 template <class in>
-KFold<in>::Kfold(int _k, in _beg, in _end) : beginning(_beg), end(_end), K(_k) {
+kfold::KFold<in>::KFold(int _k, in _beg, in _end) : beginning(_beg), end(_end), K(_k) {
   if (K <= 0) {
     throw std::runtime_error("The value of K is invalid");
   }
   int fold = 0;
-  for (in i = beg; i != end; ++i) {
+  for (in i = this->beginning; i != end; ++i) {
     this->folds.push_back(++fold);
     if (fold = K) {
       fold = 0;
     }
   }
   if (!K) {
-    throw std::rutime_error("This value of K is invalid");
+    throw std::runtime_error("This value of K is invalid");
   }
 
   std::random_shuffle(this->folds.begin(), this->folds.end());
@@ -21,7 +21,7 @@ KFold<in>::Kfold(int _k, in _beg, in _end) : beginning(_beg), end(_end), K(_k) {
 
 template <class in>
 template <class out>
-void KFold<in>::getFold(int foldNo, out training, out testing) {
+void kfold::KFold<in>::get_fold(int foldNo, out training, out testing) {
   int k = 0;
   in i = this->beginning;
 
