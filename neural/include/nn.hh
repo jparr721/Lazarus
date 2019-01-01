8f27dd281ccab296f5b53059cf2dcf1b582fccf0
Jarred Parr <jparr721@gmail.com>
2019-01-01 14:51:59 -0500

    working on nn implementation

--- neural/include/nn.hh
+++ neural/include/nn.hh
@@ -2,6 +2,8 @@
 
 #include <Eigen/Dense>
 
+#include <tuple>
+
 
 class MultiLayerPerceptron {
   public:
@@ -9,8 +11,7 @@ class MultiLayerPerceptron {
         int n_hidden, int l2, int epochs, float eta, bool shuffle, int minibatch_size, int seed);
     ~MultiLayerPerceptron() = default;
 
-    float forward_propagate(Eigen::Vector1f input_layer);
-    float predict(Eigen::Vector1f input_layer);
+    float predict(Eigen::MatrixXf input_layer);
     MultiLayerPerceptron& fit(
         Eigen::MatrixXf X_train, Eigen::Vector1f y_train, Eigen::MatrixXf X_valid, Eigen::Vector1f y_valid);
   private:
@@ -26,5 +27,8 @@ class MultiLayerPerceptron {
 
     float sigmoid(float z);
     float compute_cost(Eigen::Vector1d class_labels, Eigen::MatrixXf output);
-    Eigen::Vector1d onehot(Eigen::Vector1f class_labels, int n_classes);
+
+    std::tuple<float, float, float, float>
+      forward_propagate(Eigen::MatrixXf input_layer);
+    Eigen::MatrixXf onehot(Eigen::Vector1f class_labels, int n_classes);
 };
--- neural/nn.cc
+++ neural/nn.cc
@@ -1,5 +1,34 @@
 #include <neural/nn.hh>
+#include <cmath>
+#include <random>
 
 MultiLayerPerceptron::MultiLayerPerceptron(
   int n_hidden, int l2, int epochs, float eta, bool shuffle, int minibatch_size, int seed):
   n_hidden(n_hidden), l2(l2), epochs(epochs), eta(eta), shuffle(shuffle), minibatch_size(minibatch_size), seed(seed) {}
+
+float MultiLayerPerceptron::sigmoid(float z) {
+  return 1f / (1f + std::exp(-z));
+}
+
+Eigen::MatrixXf MultiLayerPerceptron::onehot(Eigen::MatrixXf class_labels, int n_classes) {
+  Eigen::MatrixXf (class_labels.cols());
+}
+
+float MultiLayerPerceptron::predict(Eigen::MatrixXf input_layer) {
+  std::tuple<
+    Eigen::Vectorxf,
+    Eigen::VectorXf,
+    Eigen::VectorXf,
+    Eigen::VectorXf> propagated_ouput =
+    this->forward_propagate(input_layer);
+
+  Eigen::VectorXf z_output = std::get<2>(propagated_ouput);
+
+  MatrixXf::Index max_row, max_col;
+  return z_output.maxCoeff(&max_row, &max_col);
+}
+
+std::tuple<float, float, float, float>
+MultiLayerPerceptron::forward_propagate(Eigen::MatrixXf input_layer) {
+
+}
